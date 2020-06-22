5136985fb2341d22282cfbc694c219f8f35c64e3
johannaengman <55276333+johannaengman@users.noreply.github.com>
2020-06-22 11:54:26 +0200

    Add files via upload

---
+++ lab0/lab0.py
@@ -0,0 +1,26 @@
+# A first program
+a=1
+b=2
+c = a / (b + 1)
+text = 'Result:'
+print(text, c)
+
+
+
+for i in [1, 2, 3, 4, 5, 6]:
+    print(i)
+print('Done')
+
+for i in [1, 2, 3, 4, 5, 6]:
+    if i % 2 == 0:
+        print('Even:', i)
+    else:
+        print('Odd:', i)
+print('Done')
+
+
+iliad = """Sing, O goddess, the anger of Achilles son of
+   Peleus, that brought countless ills upon the Achaeans."""
+
+iliad2 = 'Sing, O goddess, the anger of Achilles son of \
+    Peleus, that brought countless ills upon the Achaeans.'
